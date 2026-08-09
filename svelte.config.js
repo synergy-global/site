57a19a258f6ca59720ebceb228d2176f41c3ea3e
Leon mckenzie <djrowanater@gmail.com>
2026-08-09 14:44:55 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
-			// Hardcode this while testing the github.io/site link
-			base: '/site'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
